2f23a5ce31356e8f612f4e7747f6a2eb7471b117
default <trisha.ray@stu.ocsb.ca>
2025-05-12 16:57:14 +0000

    js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@
 // This file contains the JS functions for index.html
 'use strict'
 
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+ //This function multiples numbers
+
 function calculate() {
 
  // input
